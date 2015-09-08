d21b1a8e6e5655fdea644857ce3a5a17b43be077
PLR <51248199+plredmond@users.noreply.github.com>
2015-09-08 11:50:08 -0700

    post-release updated example

--- GPipe-Test/src/Main.hs
+++ GPipe-Test/src/Main.hs
@@ -1,82 +1,110 @@
-{-# LANGUAGE ScopedTypeVariables, TypeFamilies, PackageImports #-}
+{-# LANGUAGE ScopedTypeVariables, PackageImports, FlexibleContexts, TypeFamilies #-}
 module Main where
 
-import qualified Control.Monad.IO.Class as MIO
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Exception (MonadException)
+import Control.Applicative
+import Control.Monad
+import "transformers" Control.Monad.IO.Class
 
-import qualified Graphics.GPipe.Context as C
-import qualified Graphics.GPipe.Buffer as B
-import qualified Graphics.GPipe.Format as F
-import qualified Graphics.GPipe.FragmentStream as FS
-import qualified Graphics.GPipe.FrameBuffer as FB
-import qualified Graphics.GPipe.PrimitiveArray as PA
-import qualified Graphics.GPipe.PrimitiveStream as PS
-import qualified Graphics.GPipe.Shader as S
+import Graphics.GPipe
+import qualified "GPipe-GLFW" Graphics.GPipe.Context.GLFW as GLFW
+import qualified "JuicyPixels" Codec.Picture as Juicy
+import qualified "JuicyPixels" Codec.Picture.Types as Juicy
+import "linear" Linear
 
-import qualified Graphics.GPipe.Context.GLFW as Ctx
-import qualified "GLFW-b" Graphics.UI.GLFW as GLFW
+main =
+  runContextT GLFW.newContext (ContextFormatColorDepth SRGB8 Depth16) $ do
+    -- Create vertex data buffers
+    positions :: Buffer os (B2 Float) <- newBuffer 4
+    normals   :: Buffer os (B3 Float) <- newBuffer 6
+    tangents  :: Buffer os (B3 Float) <- newBuffer 6
+    writeBuffer positions 0 [V2 1 1, V2 1 (-1), V2 (-1) 1, V2 (-1) (-1)]
+    writeBuffer normals 0 [V3 1 0 0, V3 (-1) 0 0, V3 0 1 0, V3 0 (-1) 0, V3 0 0 1, V3 0 0 (-1)]
+    writeBuffer tangents 0 [V3 0 1 0, V3 0 (-1) 0, V3 0 0 1, V3 0 0 (-1), V3 (-1) 0 0, V3 1 0 0]
 
-main :: IO ()
-main = C.runContextT Ctx.newContext (F.ContextFormatColor F.RGB8) $ do
-    myBuffer :: B.Buffer os (B.B3 Float) <- B.newBuffer 3
-    shader <- S.compileShader myShader
-    myLoop shader myBuffer
+    -- Make a Render action that returns a PrimitiveArray for the cube
+    let makePrimitives = do
+          pArr <- newVertexArray positions
+          nArr <- newVertexArray normals
+          tArr <- newVertexArray tangents
+          let sideInstances = zipVertices (,) nArr tArr
+          return $ toPrimitiveArrayInstanced TriangleStrip (,) pArr sideInstances
 
-type MyShader os ds = S.Shader
-    os
-    (F.ContextFormat F.RGBFloat ds)
-    (FS.ViewPort, PA.PrimitiveArray PA.Triangles (B.B3 Float))
-    ()
+    -- Load image into texture
+    Right (Juicy.ImageYCbCr8 image) <- liftIO $ Juicy.readImage "image.jpg"
+    let size = V2 (Juicy.imageWidth image) (Juicy.imageHeight (image))
+    tex <- newTexture2D SRGB8 size maxBound -- JPG converts to SRGB
+    writeTexture2D tex 0 0 size $ Juicy.pixelFold getJuicyPixel [] image
+    generateTexture2DMipmap tex
 
-myShader :: MyShader os ds
-myShader = do
-    primitives <- PS.toPrimitiveStream snd
-    frags <- FS.rasterize
-        (\(v,_) -> (FS.FrontAndBack, v, FS.DepthRange 0 1))
-        (fmap (\(x,y,z) -> ((x,y,z,1),())) primitives)
-    FB.drawContextColor
-        (fmap (const (255,255,255)) frags)
-        (const (FB.ContextColorOption FB.NoBlending (True,True,True)))
+    -- Create a buffer for the uniform values
+    uniform :: Buffer os (Uniform (V4 (B4 Float), V3 (B3 Float))) <- newBuffer 1
 
-myLoop ::
-    ( MonadException m
-    , MonadIO m
-    , F.ContextColorFormat c
-    , Num rr , Num gg , Num bb
-    , F.Color c Float ~ (rr, gg, bb)
-    , Fractional t , Fractional t1 , Fractional t2
-    , B.HostFormat a ~ (t, t1, t2)
-    )
-    => ( (FS.ViewPort, PA.PrimitiveArray PA.Triangles a)
-       -> C.Render os (F.ContextFormat c ds) ()
-       )
-    -> B.Buffer os a
-    -> C.ContextT os (F.ContextFormat c ds) m b
-myLoop s b = do
-    B.writeBuffer [(-0.9,-0.9,0.3), (0.0,0.9,0.3),(0.9,-0.9,0.3)] 0 b
-    Just t <- MIO.liftIO GLFW.getTime
-    MIO.liftIO $ print t
-    C.render $ myRender t s b
-    C.swapContextBuffers
-    myLoop s b
+    -- Create the shader
+    shader <- compileShader $ do
+      sides <- fmap makeSide <$> toPrimitiveStream primitives
+      (modelViewProj, normMat) <- getUniform (const (uniform, 0))
+      let filterMode = SamplerFilter Linear Linear Linear (Just 4)
+          edgeMode = (pure ClampToEdge, undefined)
+          projectedSides = proj modelViewProj normMat <$> sides
+      samp <- newSampler2D (const (tex, filterMode, edgeMode))
 
-myRender :: forall b os c ds t t1 t2 a.
-    ( F.ContextColorFormat c
-    , Num t2, Num t1, Num t
-    , F.Color c Float ~ (t, t1, t2)
-    )
-    => Double
-    -> ( (FS.ViewPort, PA.PrimitiveArray PA.Triangles a)
-       -> C.Render os (F.ContextFormat c ds) b
-       )
-    -> B.Buffer os a
-    -> C.Render os (F.ContextFormat c ds) b
+      fragNormalsUV <- rasterize rasterOptions projectedSides
+      let litFrags = light samp <$> fragNormalsUV
+          litFragsWithDepth = withRasterizedInfo
+              (\a x -> (a, getZ $ rasterizedFragCoord x)) litFrags
+          colorOption = ContextColorOption NoBlending (pure True)
+          depthOption = DepthOption Less True
 
-myRender now s b = do
-    FB.clearContextColor (0,0,0)
-    size <- C.getContextBuffersSize
-    varr <- PA.newVertexArray b
-    s (FS.ViewPort (0,0) size, PA.toPrimitiveArray PA.TriangleList varr)
+      drawContextColorDepth (const (colorOption, depthOption)) litFragsWithDepth
+
+    -- Run the loop
+    loop shader makePrimitives uniform 0
+
+loop shader makePrimitives uniform angle = do
+  -- Write this frames uniform value
+  size@(V2 w h) <- getContextBuffersSize
+  let modelRot = fromQuaternion (axisAngle (V3 1 0.5 0.3) angle)
+      modelMat = mkTransformationMat modelRot (pure 0)
+      projMat = perspective (pi/3) (fromIntegral w / fromIntegral h) 1 100
+      viewMat = mkTransformationMat identity (- V3 0 0 5)
+      viewProjMat = projMat !*! viewMat !*! modelMat
+      normMat = modelRot
+  writeBuffer uniform 0 [(viewProjMat, normMat)]
+
+  -- Render the frame and present the results
+  render $ do
+    clearContextColor 0 -- Black
+    clearContextDepth 1 -- Far plane
+    prims <- makePrimitives
+    shader $ ShaderEnvironment prims (FrontAndBack, ViewPort 0 size, DepthRange 0 1)
+  swapContextBuffers
+
+  closeRequested <- GLFW.windowShouldClose
+  unless closeRequested $
+    loop shader makePrimitives uniform ((angle + 0.005) `mod''` (2*pi))
+
+getJuicyPixel xs _x _y pix =
+  let Juicy.PixelRGB8 r g b = Juicy.convertPixel pix in V3 r g b : xs
+
+getZ (V4 _ _ z _) = z -- Some day I'll learn to use lenses instead...
+
+data ShaderEnvironment = ShaderEnvironment
+  { primitives :: PrimitiveArray Triangles (B2 Float, (B3 Float, B3 Float))
+  , rasterOptions :: (Side, ViewPort, DepthRange)
+  }
+
+-- Project the sides coordinates using the instance's normal and tangent
+makeSide (p@(V2 x y), (normal, tangent)) =
+  (V3 x y 1 *! V3 tangent bitangent normal, normal, uv)
+  where bitangent = cross normal tangent
+        uv = (p + 1) / 2
+
+-- Project the cube's positions and normals with ModelViewProjection matrix
+proj modelViewProj normMat (V3 px py pz, normal, uv) =
+  (modelViewProj !* V4 px py pz 1, (fmap Flat $ normMat !* normal, uv))
+
+-- Set color from sampler and apply directional light
+light samp (normal, uv) =
+  sample2D samp SampleAuto Nothing Nothing uv * pure (normal `dot` V3 0 0 1)
 
 -- eof
