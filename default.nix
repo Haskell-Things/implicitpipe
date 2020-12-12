cef4b4c4156acd1313f7fe303fa725bb104cc170
Richard Marko <srk@48.io>
2020-12-12 15:16:55 +0100

    Update overlays and and nix files to include hint wrapper

--- default.nix
+++ default.nix
@@ -1,1 +1,37 @@
-(import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; }).haskellPackages.callCabal2nix "implicitpipe" ./. {}
+{ pkgs ? import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; }}:
+
+let
+  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+
+  myGhc = pkgs.haskellPackages.ghcWithPackages (ps: with pkgs.haskellPackages; [
+    implicit
+    linear
+  ]);
+
+  pkg =
+    #pkgs.haskell.lib.buildFromSdist (
+    #pkgs.haskell.lib.justStaticExecutables (
+      pkgs.haskellPackages.callCabal2nix "implicitpipe" src {}
+    #)
+    #)
+    ;
+
+in
+{
+  passthru = { inherit pkg; };
+  # based on https://github.com/haskell-hint/hint/issues/79
+  wrapper = pkgs.stdenv.mkDerivation {
+      name = "implicitpipe-wrapped";
+      unpackPhase = "true";
+      buildInputs = [ myGhc pkg pkgs.makeWrapper ];
+      buildPhase = ''
+        # We need to provide the Haskell interpreter (hint) with the location of the ghc lib dir and the package db
+        mkdir -p $out/bin
+        ln -s ${pkg}/bin/implicitanim $out/bin/implicitanim
+        ln -s ${pkg}/bin/implicitview $out/bin/implicitview
+        wrapProgram $out/bin/implicitview \
+          --set GHC_PACKAGE_PATH "${myGhc}/lib/${myGhc.meta.name}/package.conf.d"
+        '';
+      installPhase = "echo nothing to install";
+  };
+}
--- overlay.nix
+++ overlay.nix
@@ -1,16 +1,36 @@
 (self: super:
+let
+  gpipeSrc = super.fetchFromGitHub {
+    # fork due to resizeBuffer PR https://github.com/tobbebex/GPipe-Core/pull/76
+    # owner = "tobbebex";
+    owner = "sorki";
+    repo = "GPipe-Core";
+    rev = "86a7b29014e7ebfb24ac17d5afcd877a38a1fbd5";
+    sha256 = "08mvgygiq6i6vfjak4pq3cz1w3scvwv10igxn4vz6mna5fq6mnxz";
+  };
+
+  # until 2.0
+  gpipeGlfwSrc = super.fetchFromGitHub {
+     owner = "plredmond";
+     repo = "GPipe-GLFW";
+     rev = "83d26eb7b41d67f5ac6fbd1bd8758d72c660e039";
+     sha256 = "0fg60amvp2v37cwmvfa0n7if1ppisjjh3bknmrr17m7fbfbbxlhq";
+  };
+in
   ({
    haskellPackages = super.haskellPackages.override (old: {
       overrides = super.lib.composeExtensions (old.overrides or (_: _: {}))
         (hself: hsuper: {
-           GPipe = super.haskell.lib.doJailbreak hsuper.GPipe;
-           GPipe-GLFW = super.haskell.lib.doJailbreak hsuper.GPipe-GLFW;
+           #GPipe = super.haskell.lib.doJailbreak hsuper.GPipe;
+           GPipe = hsuper.callCabal2nix "GPipe" "${gpipeSrc}/GPipe-Core" {};
+           GPipe-GLFW = hsuper.callCabal2nix "GPipe-GLFW" ("${gpipeGlfwSrc}/GPipe-GLFW") {};
+
            # until > 3.0.2 is out
            implicit = hsuper.callCabal2nix "implicit" (super.fetchFromGitHub {
               owner = "colah";
               repo = "ImplicitCAD";
-              rev = "5ddd75006cc8f1d6a0e184f56c3714a9097ce6b0";
-              sha256 = "0mmhw9xwhlp5aa3ls44m7v7lv6l8h548w49hz8rn55r0nfpzy5z7";
+              rev = "8dff5531cdc4d9ed32bf958e3945b4a3a0ef3774";
+              sha256 = "0bp797a9wlpyw2d6b4csz5ikqq3wy1qry0iabl7r7axjrhvnfp56";
            }) {};
         });
       });
