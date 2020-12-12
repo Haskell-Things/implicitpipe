30917e6e2211fcd1d254775124309c4a0e68cd94
Richard Marko <srk@48.io>
2020-12-12 15:15:35 +0100

    Add example-haskell and example-escad

---
+++ example-haskell/SomeModule.hs
@@ -0,0 +1,18 @@
+module SomeModule where
+
+import Linear
+import Graphics.Implicit
+import Graphics.Implicit.Primitives
+
+import SomeOtherModule
+
+res = 2
+
+obj = union $ [
+    cubeR 0 False (V3 20 20 14)
+  , translate (V3 20 20 20) (sphere 15)
+  , translate (V3 30 20 20) (sphere 5)
+  , translate (V3 0 0 25) (cubeR 2 False (pure 10))
+  , translate (V3 25 0 0) (cylinder2 10 4 10)
+  , translate (V3 (-25) 0 0) obj'
+  ]
---
+++ example-haskell/SomeOtherModule.hs
@@ -0,0 +1,6 @@
+module SomeOtherModule where
+
+import Graphics.Implicit
+
+obj' = (cylinder2 5 13 42)
+
